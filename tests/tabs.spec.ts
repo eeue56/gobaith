e05d9555f9618ce5e013c7239d0a688d0dc85a4b
eeue56 <enalicho@gmail.com>
2024-12-14 00:42:55 +0100

    use a better locator for tab tests

--- tests/tabs.spec.ts
+++ tests/tabs.spec.ts
@@ -3,19 +3,19 @@ import { test } from "./fixtures";
 import { getActiveTab } from "./helpers";
 
 test("the user can switch to the importer tab", async ({ context, page }) => {
-  await page.locator('button:text("Importer")').click();
+  await page.locator('.tab:text("Importer")').click();
 
   await expect(await getActiveTab(page)).toBe("Importer");
 });
 
 test("the user can switch to the graphs tab", async ({ context, page }) => {
-  await page.locator('button:text("Graphs")').click();
+  await page.locator('.tab:text("Graphs")').click();
 
   await expect(await getActiveTab(page)).toBe("Graphs");
 });
 
 test("the user can switch to the settings tab", async ({ context, page }) => {
-  await page.locator('button:text("Settings")').click();
+  await page.locator('.tab:text("Settings")').click();
 
   await expect(await getActiveTab(page)).toBe("Settings");
 });
