4ba289b60cd7ed86c04300d6d039708dc1dba0bb
eeue56 <enalicho@gmail.com>
2025-09-05 23:51:38 +0200

    add android screenshot generation

--- screenshots/generate_screenshots.ts
+++ screenshots/generate_screenshots.ts
@@ -1,4 +1,8 @@
 import {
+  _android as android,
+  AndroidDevice,
+  Browser,
+  chromium,
   _electron as electron,
   ElectronApplication,
   expect,
@@ -8,30 +12,7 @@ import { mkdtemp } from "fs/promises";
 import { generateRandomMoodValue, isPrompt, Prompt } from "../src/types";
 import { generateData } from "../src/utils/data";
 import { dateToDay } from "../src/utils/dates";
-import { changeTab } from "../tests/helpers";
-
-async function launchElectron(): Promise<{
-  electronApp: ElectronApplication;
-  page: Page;
-}> {
-  const tempUserDir = await mkdtemp("/tmp/gobaith-electron");
-  const electronApp = await electron.launch({
-    args: ["./electron/main.js", `--user-data-dir=${tempUserDir}`],
-  });
-
-  const page = await electronApp.firstWindow();
-
-  await page.locator(".current-day").first().innerHTML();
-
-  await page.locator('button:text("Importer")').click();
-  await page
-    .locator("#import-text")
-    .fill(JSON.stringify(generateData(dateToDay(new Date()))));
-
-  await page.locator("#update-import-from-text").click();
-
-  return { page: page, electronApp };
-}
+import { awaitForTitleToChange, changeTab } from "../tests/helpers";
 
 async function closeElectron(electronApp: ElectronApplication) {
   await electronApp.close();
@@ -55,7 +36,7 @@ export async function addLogEntryForScreenshot(
   await page.locator(".save-log-entry-button").first().click();
 }
 
-async function screenshotDailyTracker(page: Page) {
+async function screenshotDailyTracker(page: Page, basePath: string) {
   await changeTab(page, "JOURNAL");
 
   const promptGroups = await page.locator(".prompt-group").all();
@@ -80,27 +61,27 @@ async function screenshotDailyTracker(page: Page) {
   }
 
   await page.screenshot({
-    path: "./screenshots/images/daily_tracker.png",
+    path: `${basePath}/daily_tracker.png`,
   });
 }
 
-async function screenshotImporter(page: Page) {
+async function screenshotImporter(page: Page, basePath: string) {
   await changeTab(page, "IMPORT");
 
   await page.screenshot({
-    path: "./screenshots/images/importer.png",
+    path: `${basePath}/importer.png`,
   });
 }
 
-async function screenshotGraphDailyBar(page: Page) {
+async function screenshotGraphDailyBar(page: Page, basePath: string) {
   await changeTab(page, "GRAPH");
 
   await page.screenshot({
-    path: "./screenshots/images/graph_daily_bar.png",
+    path: `${basePath}/graph_daily_bar.png`,
   });
 }
 
-async function screenshotGraphSpiderweb(page: Page) {
+async function screenshotGraphSpiderweb(page: Page, basePath: string) {
   await changeTab(page, "GRAPH");
   await page.locator("#graph-selection").selectOption("SPIDERWEB");
 
@@ -108,11 +89,11 @@ async function screenshotGraphSpiderweb(page: Page) {
   await page.waitForTimeout(500);
 
   await page.screenshot({
-    path: "./screenshots/images/graph_spiderweb.png",
+    path: `${basePath}/graph_spiderweb.png`,
   });
 }
 
-async function screenshotGraphLineOverview(page: Page) {
+async function screenshotGraphLineOverview(page: Page, basePath: string) {
   await changeTab(page, "GRAPH");
   await page.locator("#graph-selection").selectOption("LINE_OVERVIEW");
 
@@ -120,29 +101,29 @@ async function screenshotGraphLineOverview(page: Page) {
   await page.waitForTimeout(500);
 
   await page.screenshot({
-    path: "./screenshots/images/graph_line_overview.png",
+    path: `${basePath}/graph_line_overview.png`,
   });
 }
 
-async function screenshotGraphBipolarPeriods(page: Page) {
+async function screenshotGraphBipolarPeriods(page: Page, basePath: string) {
   await changeTab(page, "GRAPH");
   await page.locator("#graph-selection").selectOption("BIPOLAR_PERIODS");
 
   await page.screenshot({
-    path: "./screenshots/images/graph_bipolar_periods.png",
+    path: `${basePath}/graph_bipolar_periods.png`,
   });
 }
 
-async function screenshotGraphTotaledBar(page: Page) {
+async function screenshotGraphTotaledBar(page: Page, basePath: string) {
   await changeTab(page, "GRAPH");
   await page.locator("#graph-selection").selectOption("TOTALED_DAILY_BAR");
 
   await page.screenshot({
-    path: "./screenshots/images/graph_totaled_bar.png",
+    path: `${basePath}/graph_totaled_bar.png`,
   });
 }
 
-async function screenshotGraphInteractiveQueries(page: Page) {
+async function screenshotGraphInteractiveQueries(page: Page, basePath: string) {
   await changeTab(page, "GRAPH");
   await page.locator("#graph-selection").selectOption("Interactive queries");
 
@@ -151,30 +132,121 @@ async function screenshotGraphInteractiveQueries(page: Page) {
   await page.waitForTimeout(500);
 
   await page.screenshot({
-    path: "./screenshots/images/graph_interactive_queries.png",
+    path: `${basePath}/graph_interactive_queries.png`,
   });
 }
 
-async function screenshotSettings(page: Page) {
+async function screenshotSettings(page: Page, basePath: string) {
   await changeTab(page, "SETTINGS");
 
   await page.screenshot({
-    path: "./screenshots/images/settings.png",
+    path: `${basePath}/settings.png`,
   });
 }
 
+type PageRunner =
+  | { kind: "Electron"; electronApp: ElectronApplication }
+  | { kind: "Android"; device: AndroidDevice }
+  | { kind: "Chromium"; browser: Browser };
+
+async function setupPage(page: Page): Promise<void> {
+  await page.locator(".current-day").first().innerHTML();
+
+  await page.locator('button:text("Importer")').click();
+  await page
+    .locator("#import-text")
+    .fill(JSON.stringify(generateData(dateToDay(new Date()))));
+
+  await page.locator("#update-import-from-text").click();
+}
+
+function getBasePath(runner: PageRunner): string {
+  switch (runner.kind) {
+    case "Chromium":
+    case "Electron": {
+      return "./screenshots/images/";
+    }
+    case "Android": {
+      return "./screenshots/android_images/";
+    }
+  }
+}
+
+async function makePage(): Promise<{ page: Page; runner: PageRunner }> {
+  if (process.env.IS_ANDROID) {
+    const devices = await android.devices();
+
+    const [device] = devices;
+    await device.installApk(
+      "/home/noah/dev/mental-health-tracker/android/app/build/outputs/apk/debug/app-debug.apk"
+    );
+    console.log("clearing...");
+    await device.shell("pm clear com.gobaith.eeue56");
+    await device.shell("am start com.gobaith.eeue56/.MainActivity");
+
+    const webview = await device.webView({
+      pkg: "com.gobaith.eeue56",
+    });
+    const page = await webview.page();
+    await awaitForTitleToChange(page);
+
+    return { page, runner: { kind: "Android", device } };
+  } else if (process.env.IS_ELETRON) {
+    const tempUserDir = await mkdtemp("/tmp/gobaith-electron");
+    const electronApp = await electron.launch({
+      args: ["./electron/main.js", `--user-data-dir=${tempUserDir}`],
+    });
+
+    const page = await electronApp.firstWindow();
+
+    return { page, runner: { kind: "Electron", electronApp } };
+  }
+
+  const browser = await chromium.launch({ headless: false });
+
+  const page = await browser.newPage({
+    screen: { width: 500, height: 1200 },
+  });
+
+  await page.goto("http://localhost:9000");
+
+  return {
+    page: page,
+    runner: { kind: "Chromium", browser },
+  };
+}
+
 async function main() {
-  const { electronApp, page } = await launchElectron();
-  await screenshotDailyTracker(page);
-  await screenshotSettings(page);
-  await screenshotGraphDailyBar(page);
-  await screenshotImporter(page);
-  await screenshotGraphSpiderweb(page);
-  await screenshotGraphLineOverview(page);
-  await screenshotGraphBipolarPeriods(page);
-  await screenshotGraphTotaledBar(page);
-  await screenshotGraphInteractiveQueries(page);
-  await closeElectron(electronApp);
+  const { runner, page } = await makePage();
+  await setupPage(page);
+
+  const basePath = getBasePath(runner);
+
+  await screenshotDailyTracker(page, basePath);
+  await screenshotSettings(page, basePath);
+  await screenshotGraphDailyBar(page, basePath);
+  await screenshotImporter(page, basePath);
+  await screenshotGraphSpiderweb(page, basePath);
+  await screenshotGraphLineOverview(page, basePath);
+  await screenshotGraphBipolarPeriods(page, basePath);
+  await screenshotGraphTotaledBar(page, basePath);
+  await screenshotGraphInteractiveQueries(page, basePath);
+
+  switch (runner.kind) {
+    case "Electron": {
+      await closeElectron(runner.electronApp);
+      break;
+    }
+    case "Android": {
+      await runner.device.close();
+      break;
+    }
+    case "Chromium": {
+      await runner.browser.close();
+      break;
+    }
+  }
+  console.log("done!");
 }
 
 main();
